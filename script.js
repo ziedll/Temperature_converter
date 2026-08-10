112e26c18ff538f3532d6ee5fa662142591de613
ziedll <112785620+ziedll@users.noreply.github.com>
2026-08-10 09:18:22 +0100

    V1 temperature converter

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-const F = document.getElementById('F');
-const C = document.getElementById('C');
-const K = document.getElementById('K');
-const bouton = document.getElementById('button');
-const U1 = document.getElementById('Unit-debut');
-const UF = document.getElementById('Unit-final');
-const T = document.getElementById('temp-initial');
-const T_final;
-bouton.addEventListener("click",() =>{
-    if(U1 === F && UF === C)
-    {
-        T_final = (T*9/5)+32;
-        T.value = T_final; 
+const bouton = document.getElementById("button");
+const U1 = document.getElementById("Unit-debut");
+const UF = document.getElementById("Unit-final");
+const T = document.getElementById("temp");
+const Resultat = document.getElementById("result");
+
+bouton.addEventListener("click", () => {
+
+    const tempSaisie = parseFloat(T.value);
+    let T_final = 0;
+    if (U1.value === "F" && UF.value === "C") {
+        T_final = (tempSaisie * 9) / 5 + 32;
+        Resultat.value = T_final.toFixed(2);
+    } 
+    else if (U1.value === "C" && UF.value === "F") {
+        T_final = ((tempSaisie - 32) * 5) / 9;
+        Resultat.value = T_final.toFixed(2);
+    } 
+    else if (U1.value === "C" && UF.value === "K") {
+        T_final = tempSaisie + 273.15;
+        Resultat.value = T_final.toFixed(2);
+    } 
+    else if (U1.value === "K" && UF.value === "C") {
+        T_final = tempSaisie - 273.15;
+        Resultat.value = T_final.toFixed(2);
+    } 
+    else if (U1.value === "F" && UF.value === "K") {
+        T_final = ((tempSaisie - 32) * 5) / 9 + 273.15;
+        Resultat.value = T_final.toFixed(2);
+    } 
+    else if (U1.value === "K" && UF.value === "F") {
+        T_final = ((tempSaisie - 32) * 5) / 9 - 273.15;
+        Resultat.value = T_final.toFixed(2);
     }
-    if(U1 === C && UF === F )
-    {
-        T_final = (T-32)*5/9;
-    }
-    if(U1 === C && UF === K){
-        T_final = T + 273.15;
-    }
-    if(U1 === K && UF === C){
-        T_final = T - 273.15;
-    }
-    if(U1 === F && UF === K){
-        T_final = (T-32)*5/9 + 273.15;
-    }
-    if(U1 === K && UF === F){
-        T_final = (T-32)*5/9 - 273.15;
-    }
-})
+    console.log(T_final);
+    
+});
